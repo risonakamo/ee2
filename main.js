809a0e25b82cc976bfe72db0c813fe31e1023037
risonakamo <hmykanae@gmail.com>
2017-05-24 22:04:20 -0400

    +sid hash selector

--- main.js
+++ main.js
@@ -2,5 +2,26 @@ window.onload=main;
 
 function main()
 {
+    ejump();
+}
 
+function ejump()
+{
+    if (window.location.hash.length<=0)
+    {
+        return;
+    }
+
+    var link=window.location.hash.slice(1);
+
+    var eboxes=document.querySelectorAll("e-box");
+
+    for (var x=0;x<eboxes.length;x++)
+    {
+        if (eboxes[x].sid==link)
+        {
+            eboxes[x].classList.add("marked");
+            return;
+        }
+    }
 }
